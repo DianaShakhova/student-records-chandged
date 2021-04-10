55115fb3990001ba6a15db664ad23922d366ab45
DianaShakhova <81420245+DianaShakhova@users.noreply.github.com>
2021-04-10 20:03:05 +0300

    Create key-value-storage.js

---
+++ chaincode/src/contracts/key-value-storage.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+const { ClientIdentity } = require('fabric-shim');
+
+class KeyValueStorage extends Contract {
+  constructor() {
+    super('org.fabric.KeyValueStorage');
+  }
+
+  async createStudentRecord(ctx, studentEmail, fullName) {
+    const identity = new ClientIdentity(ctx.stub);
+    if(identity.cert.subject.organizationalUnitName !== 'admin'){
+      throw new Error("User does not have access to the function.");
+    }
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if(!!recordAsBytes && recordAsBytes.toString().length !== 0) { 
+      throw new Error("Student with this email already exists.");
+    }
+    const recordExample = {
+      fullName: fullName,
+      semesters: []
+    }
+    const newRecordInBytes = Buffer.from(JSON.stringify(recordExample));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+    return JSON.stringify(recordExample,null,2);
+  }
+
+  async getStudentRecord(ctx, studentEmail) {
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if (!recordAsBytes || recordAsBytes.toString().length === 0) {
+      throw new Error("Student with this email does not exist.");
+    }
+    return JSON.parse(recordAsBytes.toString());
+  }
+
+  async addSubjectToStudentRecord(ctx, studentEmail, semesterNumber, subjectName) {
+    const identity = new ClientIdentity(ctx.stub);
+    if(identity.cert.subject.organizationalUnitName !== 'admin'){
+      throw new Error("User does not have access to the function.");
+    }
+    const recordAsObject = await getStudentRecord(ctx, studentEmail);
+    if (!recordAsObject.semesters[semesterNumber]) {
+      recordAsObject.semesters[semesterNumber] = {};
+    }
+    recordAsObject.semesters[semesterNumber][subjectName] = {
+      lector: identity.cert.subject.commonName,
+      themes: []
+    }
+    const newRecordInBytes = Buffer.from(JSON.stringify(recordAsObject));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+    return JSON.stringify(recordAsObject,null,2);
+  }
+
+  async addGradeToStudentRecord(ctx, studentEmail, semesterNumber, subjectName, themeTitle, themeRating) {
+    const identity = new ClientIdentity(ctx.stub);
+    if(identity.cert.subject.organizationalUnitName !== 'admin'){
+      throw new Error("User does not have access to the function.");
+    }
+    const recordAsObject = await this.getStudentRecord(ctx, studentEmail);
+    if (!recordAsObject.semesters[semesterNumber]?.[subjectName]) {
+      throw new Error("This student does not have this subject in this semester.");
+    }
+    recordAsObject.semesters[semesterNumber][subjectName].themes.push([
+      {
+        title: themeTitle,
+        rating: themeRating,
+        date: Date.now()
+      }
+    ]);
+    // getTxTimestamp() => Returns the timestamp when the transaction was created. This is taken from the transaction ChannelHeader, therefore it will indicate the client's timestamp, and will have the same value across all endorsers. Object returned: { seconds: [Long] { low: [int32], high: [int32], unsigned: [bool] }, nanos: [int32] }
+    const newRecordInBytes = Buffer.from(JSON.stringify(recordAsObject));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+    return JSON.stringify(recordAsObject,null,2);
+  }
+
+  async getAllStudentGrades(ctx, studentEmail) {
+    const recordAsObject = await this.getStudentRecord(ctx, studentEmail);
+    return JSON.stringify(recordAsObject.semesters, null, 2);
+  }
+
+  async getAllStudentGradesForSemester(ctx, studentEmail, semester) {
+    const recordAsObject = await this.getStudentRecord(ctx, studentEmail);
+    return JSON.stringify(recordAsObject.semesters[semester] || [], null, 2);
+  }
+}
+
+module.exports = KeyValueStorage;
